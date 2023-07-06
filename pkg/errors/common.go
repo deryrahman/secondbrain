5a14214b177c25545acc5175638d0d41ca342e83
Dery Rahman Ahaddienata <dery.ra@gmail.com>
2023-07-06 21:55:47 +0700

    refactor: move error wrap to common

--- pkg/errors/common.go
+++ pkg/errors/common.go
@@ -1,7 +1,17 @@
 package errors
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 func As(err error, target any) bool {
 	return errors.As(err, target)
+}
+
+func Wrap(err error) error {
+	if err == nil {
+		return nil
+	}
+	return fmt.Errorf("%w", err)
 }
